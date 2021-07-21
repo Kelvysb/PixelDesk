019d6051cda24320effcb7dbd1afe4b715184e42
kelvys B. Pantaleão <contato@kelvysb.com>
2021-07-21 14:21:47 -0300

    Adjust

--- PixelDesk/pixelDesk.py
+++ PixelDesk/pixelDesk.py
@@ -166,6 +166,6 @@ if __name__ == '__main__':
     abspath = os.path.abspath(__file__)
     dname = os.path.dirname(abspath)
     os.chdir(dname)   
-    Window.fullscreen = False
+    Window.fullscreen = True
     Window.show_cursor = False
     PixelDesk().run()
